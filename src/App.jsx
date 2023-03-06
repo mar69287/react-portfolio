693ddcda0d6908b8c37ff2d87e71bc41f2c81f71
mar69287 <marcoa.ruiz97@gmail.com>
2023-03-06 11:50:32 -0800

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/Header/Header';
+
+
+export default function App() {
+
+    return (
+        <div className="App">
+            <NavBar />
+            <main>
+                <header />
+            </main>
+        </div>
+    );
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+import logo from '../../images/logo.png'
+import { useEffect } from 'react'
+
+export default function NavBar() {
+
+    useEffect(() => {
+        let hamburgerIcon = document.querySelector('.hamburger-icon');
+        let navUl = document.querySelector('.nav-ul');
+
+        hamburgerIcon.addEventListener('click', () => {
+            navUl.classList.toggle('active');
+        });
+
+        let navLinks = document.querySelectorAll('.nav-ul li a');
+        navLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                navUl.classList.remove('active');
+            });
+        });
+    }, []);
+
+    return (
+        <nav>
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <ul className='nav-ul'>
+                <li><a href="#" >Home</a></li>
+                <li><a href="#projects" >Projects</a></li>
+                <li><a href="#about" >About Me</a></li>
+                <li><a href="#" >Resume</a></li>
+                <li><a href="#contact" >Contact Me</a></li>
+            </ul>
+            <a class="hamburger-icon">
+                <i class="fa fa-bars"></i>
+            </a>
+        </nav>
+    )
+}
